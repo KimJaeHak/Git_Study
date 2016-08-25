10ca1a948791aa7774fe4d48b23af3ffedafc044
jaehak.kim <09saver@naver.com>
2016-08-26 01:26:14 +0900

    브랜치 생성을 위한 커밋

--- MyFolder/Program.cs
+++ MyFolder/Program.cs
@@ -12,7 +12,7 @@ namespace WienerModeling
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void TestMain()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
